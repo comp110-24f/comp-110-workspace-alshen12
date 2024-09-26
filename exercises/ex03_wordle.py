55784dd4163a8c82dc32ad8db637dde7f4574b2c
alshen12 <alshen@ad.unc.edu>
2024-09-26 01:08:55 -0400

    modified:   exercises/ex03_wordle.py

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -6,16 +6,20 @@ __author__ = "730630720"
 def main(secret: str) -> None:
     """The entrypoint of the program and main game loop."""
     turn: int = 1
-    while turn < 7:
+    while (
+        turn < 7
+    ):  # runs the main program until the turns counter hits over 6, in which the game ends
         print(f"=== Turn {turn}/6 ===")
-        guess = input_guess(secret_word_len=len(secret))
-        print(emojified(guess=guess, secret_word=secret))
+        guess = input_guess(secret_word_len=len(secret))  #
+        print(
+            emojified(guess=guess, secret_word=secret)
+        )  # utlizes user input to check correctness using emojified function
         if secret == guess:
             print(f"You won in {turn}/6 turns!")
-            break
+            break  # breaks out of loop once user wins
         turn += 1
 
-    if turn >= 7:
+    if turn >= 7:  # >= to catch any potential time where turn somehow becomes over 7
         print("X/6 - Sorry, try again tomorrow!")
 
 
@@ -31,10 +35,10 @@ def contains_char(secret_word: str, char_guess: str) -> bool:
     """Sorts through each letter in the secret word using the char guess to see if there is a match."""
     assert len(char_guess) == 1
     idx: int = 0
-    charinword = False
-    while idx < len(secret_word):
+    charinword = False  # assumed false initially just in case charinword does not exist
+    while idx < len(secret_word):  # sorts through word
         if char_guess == secret_word[idx]:
-            charinword = True
+            charinword = True  # bool value becomes true once the character at the slot in the guess matches up with a char in secret word.
         idx += 1
     return charinword
 
@@ -50,13 +54,11 @@ def emojified(guess: str, secret_word: str) -> str:
         if guess[idx] == secret_word[idx]:
             wordle_guess += GREEN_BOX
         elif guess[idx] != secret_word[idx]:
-            idx_2 = 0
-            while idx_2 < len(guess):
-                if guess[idx] == secret_word[idx_2]:
-                    wordle_guess += YELLOW_BOX
-                    break
-                idx_2 += 1
-            else:
+            if (
+                contains_char(secret_word=secret_word, char_guess=guess[idx]) == True
+            ):  # calls function comparing characters in the guess at that slot to characters in the actual word.
+                wordle_guess += YELLOW_BOX
+            else:  # if the guess at idx doesn't match up with guess in secret word AND it isn't found anywhere in the word, then white box
                 wordle_guess += WHITE_BOX
         idx += 1
     return wordle_guess
